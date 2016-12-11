5764e4b765a06219c2be017cdb0a7f6577870131
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2016-12-11 17:29:10 +0100

    Refactor working.

--- __init__.py
+++ __init__.py
@@ -1,3 +1,7 @@
+import bpy
+from bpy_extras.io_utils import ExportHelper
+import bmesh
+import os
 from .mos import level, materials, meshes, models
 
 bl_info = {
@@ -10,12 +14,101 @@ bl_info = {
     "category":     "Import-Export"
 }
 
+class ExportLevelFormat(bpy.types.Operator, ExportHelper):
+    bl_idname = "export_scene.json"
+    bl_label = "General level format"
+    bl_options = {'PRESET'}
+    filename_ext = ".json"
+
+    def execute(self, context):
+        level.write(os.path.dirname(self.filepath))
+
+class ExportMaterialsFormat(bpy.types.Operator, ExportHelper):
+    bl_idname = "export_mesh.material"
+    bl_label = "Mo material format"
+    bl_options = {'PRESET'}
+    filename_ext = ".material"
+
+    def execute(self, context):
+        materials.write(os.path.dirname(self.filepath))
+        return {'FINISHED'}
+
+class ExportMeshesFormat(bpy.types.Operator, ExportHelper):
+    bl_idname = "export_mesh.mesh"
+    bl_label = "Mo mesh format"
+    bl_options = {'PRESET'}
+    filename_ext = ".mesh"
+
+    def execute(self, context):
+        meshes.write(os.path.dirname(self.filepath), context.selected_objects)
+        return {'FINISHED'}
+
+class ExportEntitiesFormat(bpy.types.Operator, ExportHelper):
+    bl_idname = "export_entities.entity"
+    bl_label = "Entities"
+    bl_options = {'PRESET'}
+    filename_ext = ".entity"
+
+    def execute(self, context):
+        blender_objects = [o for o in context.scene.objects if not o.parent and o.type in {"MESH", "EMPTY"}]
+        directory = os.path.dirname(self.filepath)
+        entities.write(directory, blender_objects)
+        return {'FINISHED'}
+
+
+def export_level_menu_func(self, context):
+    self.layout.operator(ExportLevelFormat.bl_idname, text="General level format(.json)")
+
+
+def export_materials_menu_func(self, context):
+    self.layout.operator(ExportMaterialsFormat.bl_idname, text="Mo material format (.material)")
+
+
+def export_meshes_menu_func(self, context):
+    self.layout.operator(ExportMeshesFormat.bl_idname, text="Mo mesh format (.mesh)")
+
+
+def export_models_menu_func(self, context):
+    self.layout.operator(ExportModelsFormat.bl_idname, text="Model format(.model)")
+
+
+def export_entities_level_func(self, context):
+    self.layout.operator(ExportEntitiesFormat.bl_idname, text="Entity format (.entity)")
+
+
+
+class ExportModelsFormat(bpy.types.Operator, ExportHelper):
+    bl_idname = "export_models.model"
+    bl_label = "Model format"
+    bl_options = {'PRESET'}
+    filename_ext = ".model"
+
+    def execute(self, context):
+        blender_objects = [o for o in context.scene.objects if not o.parent and (o.type == 'MESH' or (o.type == 'EMPTY' and o.children))]
+        dir = os.path.dirname(self.filepath)
+        models.write(dir, blender_objects)
+
+        return {'FINISHED'}
+
 
 def register():
+    bpy.utils.register_module(__name__)
 
+    bpy.types.INFO_MT_file_export.append(export_level_menu_func)
+    bpy.types.INFO_MT_file_export.append(export_materials_menu_func)
+    bpy.types.INFO_MT_file_export.append(export_meshes_menu_func)
+    bpy.types.INFO_MT_file_export.append(export_models_menu_func)
+    bpy.types.INFO_MT_file_export.append(export_entities_level_func)
 
 
 def unregister():
+    bpy.utils.unregister_module(__name__)
+
+    bpy.types.INFO_MT_file_export.remove(export_level_menu_func)
+    bpy.types.INFO_MT_file_export.remove(export_materials_menu_func)
+    bpy.types.INFO_MT_file_export.remove(export_meshes_menu_func)
+    bpy.types.INFO_MT_file_export.remove(export_models_menu_func)
+    bpy.types.INFO_MT_file_export.remove(export_entities_level_func)
 
 
 if __name__ == "__main__":
