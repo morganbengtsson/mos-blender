36a4db9dd3f8f2ffad3787237d06e524dcf1d543
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2016-12-15 10:55:44 +0100

    Fix

--- mos/materials.py
+++ mos/materials.py
@@ -1,5 +1,6 @@
 import bpy
 import struct
+import json
 
 
 def write(dir):
@@ -10,17 +11,27 @@ def write(dir):
 
         print('Exporting: ' + blender_material.name)
 
-        material = {"ambient": blender_material.diffuse_color * blender_material.ambient,
-                    "diffuse": blender_material.diffuse_color,
-                    "specular": blender_material.specular_color,
+        material = {"ambient": tuple(blender_material.diffuse_color * blender_material.ambient),
+                    "diffuse": tuple(blender_material.diffuse_color),
+                    "specular": tuple(blender_material.specular_color),
                     "opacity": blender_material.alpha,
-                    "specular_exponent": float(blender_material.specular_hardness)}
+                    "specular_exponent": float(blender_material.specular_hardness),
+                    "texture": blender_material.get("texture"),
+                    "normalmap" : blender_material.get("normalmap")}
+
+        print(material)
 
         material_file.write(struct.pack('fff', *material["ambient"]))
         material_file.write(struct.pack('fff', *material["diffuse"]))
         material_file.write(struct.pack('fff', *material["specular"]))
+
         material_file.write(struct.pack('f', material["opacity"]))
         material_file.write(struct.pack('f', material["specular_exponent"]))
 
         material_file.close()
         print("Wrote file: " + material_file.name)
+
+        json_file = open(dir + '/' + blender_material.name + '.material', 'w')
+        json.dump(material, json_file)
+        json_file.close()
+
