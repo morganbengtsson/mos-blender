487db175349be71ed25e416581a2c550feb31c8a
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2017-01-17 11:57:47 +0100

    check

--- mos/models.py
+++ mos/models.py
@@ -3,81 +3,88 @@ import bpy
 from . import materials, meshes
 
 
+class ObjectEncoder(json.JSONEncoder):
+    def default(self, o):
+        return o.__dict__
+
+
 class Model(object):
     def __init__(self, name=None, transform=[1, 0, 0, 0,
                                              0, 1, 0, 0,
                                              0, 0, 1, 0,
                                              0, 0, 0, 1], mesh=None,
-                 material=None, lit=True):
+                 material=None, lit=True, models=list()):
         self.name = name
         self.transform = transform
         self.mesh = mesh
         self.material = material
         self.lit = lit
+        self.models = models
 
-
-
-def to_model(blender_object, force):
-    if not blender_object:
-        return None
-
-    if blender_object.get("export_model") in {"0", "False", "false", 0}:
-        return None
-
-    if blender_object.type not in {"MESH", "EMPTY"}:
-        return None
-
-    if blender_object.parent is not None and not force:
-        if blender_object.get("entity_type") is not None:
+    @classmethod
+    def from_blender_object(cls, blender_object, force):
+        if not blender_object:
             return None
 
-    print("---")
-    print("Exporting: " + blender_object.name)
-    print("Type: " + blender_object.type)
-    print("Entity type: " + str(blender_object.get("entity_type")))
-    print("Parent: " + str(blender_object.parent))
-    model = dict()
-    model['name'] = blender_object.name
+        if blender_object.get("export_model") in {"0", "False", "false", 0}:
+            return None
 
-    transform_matrix = blender_object.matrix_local
+        if blender_object.type not in {"MESH", "EMPTY"}:
+            return None
 
-    transform = list()
-    for row in transform_matrix.col:
-        transform.extend(list(row))
+        if blender_object.parent is not None and not force:
+            if blender_object.get("entity_type") is not None:
+                return None
 
-    model["transform"] = transform
+        print("---")
+        print("Exporting: " + blender_object.name)
+        print("Type: " + blender_object.type)
+        print("Entity type: " + str(blender_object.get("entity_type")))
+        print("Parent: " + str(blender_object.parent))
 
-    if blender_object.type == "MESH":
-        model["mesh"] = blender_object.data.name
-        for modifier in blender_object.modifiers:
-            model["mesh"] += "_" + modifier.name
-        model['mesh'] += ".mesh"
+        model = Model()
 
-    if blender_object.active_material:
-        model['material'] = str(blender_object.active_material.name + ".material")
+        model.name = blender_object.name
 
-    if blender_object.get("lit") is not None:
-        model["lit"] = bool(blender_object.get("lit"))
+        transform_matrix = blender_object.matrix_local
 
-    return model
+        transform = list()
+        for row in transform_matrix.col:
+            transform.extend(list(row))
+
+        model.transform = transform
+
+        if blender_object.type == "MESH":
+            model.mesh = blender_object.data.name
+            for modifier in blender_object.modifiers:
+                model.mesh += "_" + modifier.name
+            model.mesh += ".mesh"
+
+        if blender_object.active_material:
+            model.material = str(blender_object.active_material.name + ".material")
+
+        if blender_object.get("lit") is not None:
+            model.lit = bool(blender_object.get("lit"))
+
+        return model
 
 
 def to_models(blender_objects, force):
     root = []
     for object in blender_objects:
-        model = to_model(object, force)
+        model = Model.from_blender_object(object, force)
         if model:
-            model['models'] = to_models(object.children, force)
+            model.models = to_models(object.children, force)
             root.append(model)
     return root
 
 
-def write(directory, objects, force = False):
+def write(directory, objects, force=False):
     print("Writing models.")
     models = to_models(objects, force)
     for model in models:
-        file = open(directory + '/' + model["name"] + ".model", 'w')
-        file.write(json.dumps(model))
+        file = open(directory + '/' + model.name + ".model", 'w')
+        file.write(json.dumps(model, cls=ObjectEncoder))
         file.close()
 
     print("Writing materials.")
