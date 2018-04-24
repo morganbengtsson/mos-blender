2f295385f392a5b68120a9db2aa13ec1b221d3f1
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2018-04-24 16:25:22 +0200

    Cleanup

--- mos/level.py
+++ mos/level.py
@@ -4,27 +4,25 @@ import struct
 import json
 from . import materials, entities, meshes, lights
 
+
 def write(dir, filepath, objects):
     blender_objects = [o for o in objects if not o.parent and o.type in {"MESH", "EMPTY"}]
     blender_objects = sorted(blender_objects, key=lambda x: x.name, reverse=False)
 
-    directory = dir
-
-    root = list()
-
     print("Writing level.")
+    root = list()
     for blender_object in blender_objects:
         root.append(str(blender_object.name) + "." + str(blender_object.get("entity_type") or "model"))
-    file = open(filepath, 'w')
-    file.write(json.dumps(root))
-    file.close()
+    level_file = open(filepath, 'w')
+    level_file.write(json.dumps(root))
+    level_file.close()
 
     print("Writing models.")
-    entities.write(directory, objects)
+    entities.write(dir, objects)
     print("Writing materials.")
-    materials.write(directory)
+    materials.write(dir)
     print("Writing meshes.")
-    meshes.write(directory, [o for o in objects if o.type == "MESH"])
+    meshes.write(dir, [o for o in objects if o.type == "MESH"])
 
     #print("Writing lamps.")
     #lights.write(directory)
