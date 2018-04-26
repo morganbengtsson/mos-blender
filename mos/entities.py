9400dbf825f317bff2db879355aa03d8b81acaea
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2018-04-26 12:57:34 +0200

    Array json serialization.

--- mos/entities.py
+++ mos/entities.py
@@ -1,11 +1,8 @@
 import json
 import bpy
+import idprop
+
 from . import materials, meshes, light_data
-
-
-class ObjectEncoder(json.JSONEncoder):
-    def default(self, o):
-        return o.__dict__
 
 
 def mesh_name(blender_object):
@@ -15,42 +12,42 @@ def mesh_name(blender_object):
     return name
 
 
-class Entity(object):
-    def __init__(self):
-        self.name = None
-        self.transform = [1, 0, 0, 0,
-                          0, 1, 0, 0,
-                          0, 0, 1, 0,
-                          0, 0, 0, 1]
-        self.mesh = None
-        self.material = None
-        self.children = list()
-        self.type = "model"
-        self.id = None
-        self.light = None
+def write_file(entity, directory):
+    entity_file = open(directory + '/' + entity["name"] + "." + entity["type"], 'w')
+    entity_file.write(json.dumps(entity))
+    entity_file.close()
 
-    def write(self, directory):
-        entity_type = self.type
-        entity_file = open(directory + '/' + self.name + "." + entity_type, 'w')
-        entity_file.write(json.dumps(self, cls=ObjectEncoder))
-        entity_file.close()
 
-    def file_name(self):
-        return str(self.name) + "." + str(self.type)
+def file_name(entity):
+    return str(entity["name"] + "." + str(entity["type"]))
 
 
 def write_entity(blender_object, directory):
     if blender_object.type not in {"MESH", "EMPTY", "LAMP"}:
         print("Not supported")
     else:
-        entity = Entity()
+        entity = dict()
+        entity["name"] = None
+        entity["transform"] = [1, 0, 0, 0,
+                          0, 1, 0, 0,
+                          0, 0, 1, 0,
+                          0, 0, 0, 1]
+        entity["mesh"] = None
+        entity["material"] = None
+        entity["children"] = list()
+        entity["type"] = "model"
+        entity["id"] = None
+        entity["light"] = None
 
         keys = blender_object.keys()
         for key in keys:
             if not key.startswith("_") and not key.startswith("cycles"):
-                setattr(entity, key, blender_object[key])
+                if type(blender_object[key]) is idprop.types.IDPropertyArray:
+                    entity[key] = list(blender_object[key])
+                else:
+                    entity[key] = blender_object[key]
 
-        entity.name = blender_object.name
+        entity["name"] = blender_object.name
 
         transform_matrix = blender_object.matrix_local
 
@@ -58,30 +55,30 @@ def write_entity(blender_object, directory):
         for row in transform_matrix.col:
             transform.extend(list(row))
 
-        entity.transform = transform
+        entity["transform"] = transform
 
         extension = "model" if blender_object.type in {"MESH", "EMPTY"} else "light" if blender_object.type == "LAMP" else "model"
 
-        entity.type = blender_object.get("entity_type") or extension
+        entity["type"] = blender_object.get("entity_type") or extension
 
-        entity.id = blender_object.as_pointer()
+        entity["id"] = blender_object.as_pointer()
 
         if blender_object.type == "MESH":
-            entity.mesh = mesh_name(blender_object)
-            entity.mesh += ".mesh"
+            entity["mesh"] = mesh_name(blender_object)
+            entity["mesh"] += ".mesh"
 
         if blender_object.type == "LAMP":
-            entity.light = blender_object.data.name + ".light_data"
+            entity["light"] = blender_object.data.name + ".light_data"
 
         if blender_object.active_material:
-            entity.material = str(blender_object.active_material.name + ".material")
+            entity["material"] = str(blender_object.active_material.name + ".material")
 
         for blender_child in blender_object.children:
             entity_child = write_entity(blender_child, directory)
             if entity_child:
-                entity.children.append(entity_child.file_name())
+                entity["children"].append(file_name(entity_child))
 
-        entity.write(directory)
+        write_file(entity, directory)
         return entity
 
 
