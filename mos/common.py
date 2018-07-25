aa7add3311190f2ac7cc4a336cd9d561d4c64380
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2018-07-25 16:23:27 +0200

    Path fix.

--- mos/common.py
+++ mos/common.py
@@ -5,6 +5,6 @@ import os
 def library_path(blender_object):
     library = os.path.splitext(bpy.path.basename(bpy.context.blend_data.filepath))[0] + '/'
     if blender_object.library:
-        library, file_extension = os.path.splitext(blender_object.library.filepath)
+        library, file_extension = os.path.splitext(bpy.path.basename(blender_object.library.filepath))
         library = library + '/'
     return library
